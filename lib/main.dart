26356c6889a081770b9a2d68466837722afada32
fkn1017 <kosuke.fukano@gmail.com>
2021-01-09 13:02:10 +0900

    ホーム画面のコード修正

--- lib/main.dart
+++ lib/main.dart
@@ -47,73 +47,36 @@ class Home extends StatelessWidget {
       appBar: AppBar(title: Text('ホーム')),
       body: ListView(
         children: [
-          ListTile(
-            title: Text('画像を長押しすると、画像が前面に出てくる(GridView x Hero)。'),
-            leading: Text('1'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample1');
-            },
-          ),
-          ListTile(
-            title: Text('よくわからないサンプル'),
-            leading: Text('2'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample2');
-            },
-          ),
-          ListTile(
-            title: Text('よくわからないサンプル'),
-            leading: Text('3'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample3');
-            },
-          ),
-          ListTile(
-            title: Text('四角を長押しすると大きい四角が前面に出る。(バウンスアニメーション＋Hero)'),
-            leading: Text('4'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample4');
-            },
-          ),
-          ListTile(
-            title: Text('画像を長押しすると大きい画像が前面に出る。(バウンスアニメーション＋Hero)'),
-            leading: Text('5'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample5');
-            },
-          ),
-          ListTile(
-            title: Text('1と5の組み合わせ(GridView x Hero x バウンスアニメーション)'),
-            leading: Text('6'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample6');
-            },
-          ),
-          ListTile(
-            title:
-                Text('6＋タップ位置に応じて拡大画像の位置を変える(GridView x Hero x バウンスアニメーション)'),
-            leading: Text('7'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample7');
-            },
-          ),
-          ListTile(
-            title: Text('Sample7の背景ぼかしバージョン'),
-            leading: Text('8'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              Navigator.pushNamed(context, '/sample8');
-            },
-          ),
+          SampleListTile(1, '画像を長押しすると、画像が前面に出てくる(GridView x Hero)。'),
+          SampleListTile(2, 'よくわからないサンプル'),
+          SampleListTile(3, 'よくわからないサンプル'),
+          SampleListTile(4, '四角を長押しすると大きい四角が前面に出る。(バウンスアニメーション＋Hero)'),
+          SampleListTile(5, '画像を長押しすると大きい画像が前面に出る。(バウンスアニメーション＋Hero'),
+          SampleListTile(6, 'Sample1と5の組み合わせ(GridView x Hero x バウンスアニメーション)'),
+          SampleListTile(
+              7, 'Sample6＋タップ位置に応じて拡大画像の位置を変える(GridView x Hero x バウンスアニメーション)'),
+          SampleListTile(8, 'Sample7の背景ぼかしバージョン'),
         ],
       ),
+    );
+  }
+}
+
+class SampleListTile extends StatelessWidget {
+  SampleListTile(this.index, this.title);
+
+  final int index;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: Text('$index'),
+      trailing: Icon(Icons.arrow_forward),
+      onTap: () {
+        Navigator.pushNamed(context, '/sample$index');
+      },
     );
   }
 }
